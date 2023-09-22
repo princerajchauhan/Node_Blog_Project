ed2e6165b7505d914ce32f09550a885e20350570
Prince <princeraj.chauhan@outlook.com>
2023-09-22 23:12:54 +0530

    some styles

--- src/Spinner/Spinner.js
+++ src/Spinner/Spinner.js
@@ -1,11 +1,13 @@
 import React from "react";
 import loading from "./loadingball.gif"
+import gify from "./giphy.gif"
 
 const Spinner = () => {
     return (
-        <div style={{textAlign:'center'}}>
-            <img src={loading} alt="loading"/>
-        </div>
+        <div style={{ height: '100vh', display: 'flex' ,alignItems: 'center', flexDirection:"column" }}>
+                <img src={loading} alt="loading" />
+                <img src={gify} alt="loading" style={{marginTop:'-130px', width:'12%'}}/>
+            </div>
     )
 }
 
