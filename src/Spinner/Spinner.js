2a3a33582bd772b1f2fd0cc9c1822620a841b85b
Prince <princeraj.chauhan@outlook.com>
2023-09-22 23:18:24 +0530

    some styles

--- src/Spinner/Spinner.js
+++ src/Spinner/Spinner.js
@@ -4,9 +4,9 @@ import gify from "./giphy.gif"
 
 const Spinner = () => {
     return (
-        <div style={{ height: '100vh', display: 'flex' ,alignItems: 'center', flexDirection:"column" }}>
+        <div style={{ height: '100vh', display: 'flex' ,alignItems: 'center', flexDirection:"column", position:'relative' }}>
                 <img src={loading} alt="loading" />
-                <img src={gify} alt="loading" style={{marginTop:'-130px', width:'12%'}}/>
+                <img src={gify} alt="loading" style={{width:'200px', position:'absolute', top:"65px"}}/>
             </div>
     )
 }
